b538e26394fc3f75bf2fc56d8476c8a1c8cce823
Wolfi <halbeisen@hotmail.com>
2011-03-13 14:53:37 +0100

    added Android-JUnit-Test-Application.

---
+++ tests/src/at/roadrunner/android/test/activity/RoadrunnerTest.java
@@ -0,0 +1,25 @@
+package at.roadrunner.android.test.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+import at.roadrunner.android.Roadrunner;
+
+public class RoadrunnerTest extends ActivityInstrumentationTestCase2<Roadrunner> {
+
+	public RoadrunnerTest() {
+		super("at.roadrunner.android", Roadrunner.class);
+	}
+	
+	private Roadrunner _a;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		_a = getActivity();
+	}
+	
+	public void testPreconditions()  {
+		assertNotNull(_a);
+	}
+
+}
