4b2d65f453766c7609a9641a978dbc08748da48f
Wolfi <halbeisen@hotmail.com>
2011-03-13 14:51:50 +0100

    added Roadrunner-Activity

---
+++ src/at/roadrunner/android/Roadrunner.java
@@ -0,0 +1,15 @@
+package at.roadrunner.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Roadrunner extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
+
