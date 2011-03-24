b75e1a85613704f471f95976a6c55ef00b4d6759
Stefan Gassner <stefan.gassner@gmail.com>
2011-03-24 11:12:49 +0100

    added min SDK version (supresses waring now) renamed layout file added styles.xml for theme (and light version)

--- src/at/roadrunner/android/activity/Roadrunner.java
+++ src/at/roadrunner/android/activity/Roadrunner.java
@@ -1,15 +1,15 @@
 package at.roadrunner.android.activity;
 
+import android.R;
 import android.app.Activity;
 import android.os.Bundle;
-import at.roadrunner.android.R;
 
 public class Roadrunner extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_Roadrunner);
     }
     
     
