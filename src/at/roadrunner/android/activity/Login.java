72bf2ba0261e2f93ca0e92325474ff0eb807b56c
Stefan Gassner <stefan.gassner@gmail.com>
2011-05-18 15:05:19 +0200

    selected transportation is saved to the preferences

--- src/at/roadrunner/android/activity/Login.java
+++ src/at/roadrunner/android/activity/Login.java
@@ -2,12 +2,15 @@ package at.roadrunner.android.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 import at.roadrunner.android.R;
 
 public class Login extends Activity {
@@ -19,26 +22,36 @@ public class Login extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
+		final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		
 		String[] containers = new String[] {"LKW1", "LKW2", "LKW3", "PKW1"};
 		ArrayAdapter<String> containerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, containers);
 		containerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		
-		Spinner cbxTransportation = (Spinner) findViewById(R.id.login_container);
+		final Spinner cbxTransportation = (Spinner) findViewById(R.id.login_container);
 		cbxTransportation.setAdapter(containerAdapter);
-		
-		
-		
+		cbxTransportation.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				prefs.edit().putString("transportation", cbxTransportation.getSelectedItem().toString()).commit();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				
+			}
+		});
 	}
 	
 	public void onLoginClick(View view) {
 		EditText txtUsername = (EditText) findViewById(R.id.login_username);
 		EditText txtPassword = (EditText) findViewById(R.id.login_password);
 		
-		if (txtUsername.getText().toString().equals("xxx") && txtPassword.getText().toString().equals("y") ) {
-			startActivity(new Intent(this, Roadrunner.class));
-		} else {
-			Toast toast = Toast.makeText(this, "HAHA FALSCH GERATEN! ZONK! -> user: xxx, pwd: y", Toast.LENGTH_LONG);
-			toast.show();
-		}
+		// TODO: Authenticate through server -> Wolfy FIX THIS MAN!
+		
+		startActivity(new Intent(this, Roadrunner.class));
+		
 	}
 }
