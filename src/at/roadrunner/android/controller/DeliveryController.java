c367ebaaef3ee035ebc8876915a86eec0f9a69d7
Stefan Gassner <stefan.gassner@gmail.com>
2011-05-25 12:00:01 +0200

    added third address

--- src/at/roadrunner/android/controller/DeliveryController.java
+++ src/at/roadrunner/android/controller/DeliveryController.java
@@ -21,6 +21,7 @@ public class DeliveryController {
 		Address from = new Address("Franziskus Domig", "Susergasse 29", "6800", "Feldkirch", "Austria");
 		Address to = new Address("Matthias Schmid", "Feldkircherstraße 33a", "6820", "Frastanz", "Austria");
 		Address to1 = new Address("Stefan Gassner", "56", "6741", "Raggal", "Austria");
+		Address to2 = new Address("Florian Gopp", "Bergstraße 24", "6835", "Zwischenwasser", "Austria");
 		
 		Delivery delivery1 = new Delivery("aldkfjop34ukfdjoi3u4ldf");
 		delivery1.addItem(new Item("Notebook", 10, 20));
@@ -30,13 +31,18 @@ public class DeliveryController {
 		delivery1.setFrom(from);
 		
 		Delivery delivery2 = new Delivery("23t5124353242323");
-		delivery2.addItem(new Item("Mouse", 0, 10));
 		delivery2.addItem(new Item("Tastatur", 10, 20));
 		delivery2.setDestination(to1);
 		delivery2.setFrom(from);
+		
+		Delivery delivery3 = new Delivery("asdfasdf");
+		delivery3.addItem(new Item("Mouse", 0, 10));
+		delivery3.setDestination(to1);
+		delivery3.setFrom(to2);
 
 		_deliveries.add(delivery1);
 		_deliveries.add(delivery2);
+		_deliveries.add(delivery3);
 		return _deliveries;
 	}
 }
