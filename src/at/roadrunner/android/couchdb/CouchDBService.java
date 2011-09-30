50db303372fd0ee7292ac24c0fc9efefb5a16c88
= <=>
2011-09-30 15:49:12 +0200

    removed unused code

--- src/at/roadrunner/android/couchdb/CouchDBService.java
+++ src/at/roadrunner/android/couchdb/CouchDBService.java
@@ -1,21 +1,16 @@
 package at.roadrunner.android.couchdb;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
 import at.roadrunner.android.Config;
 
 public class CouchDBService {
 	
 	public static void startCouchDB(Context context)  {
-//		context.bindService(new Intent(Config.COUCH_SERVICE), _connection, Context.BIND_AUTO_CREATE);
 		context.startService(new Intent(Config.COUCH_SERVICE));
 	}
 	
 	public static void stopCouchDB(Context context)  {
-//		context.unbindService(_connection);
 		context.stopService(new Intent(Config.COUCH_SERVICE));
 	}
 
@@ -23,30 +18,4 @@ public class CouchDBService {
 		stopCouchDB(context);
 		startCouchDB(context);
 	}
-
-	@SuppressWarnings("unused")
-	private static class CouchDBConnection implements ServiceConnection {
-
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			// TODO Auto-generated method stub
-			
-		}
-	}
-
-	
-//	private static class CouchDBConnection implements ServiceConnection  {
-//		
-//		@Override
-//		public void onServiceConnected(ComponentName name, IBinder service) {
-//		}
-//
-//		@Override
-//		public void onServiceDisconnected(ComponentName name) {
-//		}
-//	}
 }
