416d18217c694e66107d67440f45f601134123a4
Wolfy <halbeisen@hotmail.com>
2011-10-05 13:28:30 +0200

    changed service intervals

--- src/at/roadrunner/android/Config.java
+++ src/at/roadrunner/android/Config.java
@@ -46,9 +46,9 @@ public class Config {
 	public static final String TEMPERATUR_SENSOR_URL_1 = PROTOCOL + ROADRUNNER_SERVER_IP + ":" + "10001"; 
 
 	// Service Settings
-	public static final long MONITORING_SERVICE_INTERVAL = 300000;
-	public static final long LOGGING_SERVICE_INTERAL = 300000;
-	public static final long REPLICATOR_SERVICE_INTERAL = 10000;
+	public static final long MONITORING_SERVICE_INTERVAL = 5000;
+	public static final long LOGGING_SERVICE_INTERAL = 5000;
+	public static final long REPLICATOR_SERVICE_INTERAL = 30000;
 	
 	public static final String DEFAULT_TRANSPORTATION = "UNKNOWN";
 	
